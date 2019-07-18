a196c054f37d6395f266f7a109f3b4bfdd950ecc
semipan <28053767@qq.com>
2019-07-18 14:50:56 +0800

    完成非洲国际和地区的统计

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo '<!doctype html>
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
     <title>文档说明</title>
     <style>
     blockquote{
